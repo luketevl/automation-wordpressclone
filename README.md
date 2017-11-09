615e6c4524327a3ca2f52b4a7013d08ce86afafd
luketevl <luketevl@gmail.com>
2017-11-09 12:04:46 -0200

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# PRE REQUIRIMENTS
+
+- wp cli
+	- https://developer.wordpress.org/cli/commands/
+
+# STRUCTURE
+
+- target
+- folder
+	- folder
+		- script to install
+
+# USE
+- Set your database password in file `passwword.txt`
+
+# Command line
+
+```shell
+./install.sh "namesClients" "urlsite"
+```
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Creating new wordpress
+CLIENTS_NAMES=$1
+TARGET="lpmodelo"
+for client in $CLIENTS_NAMES
+do
+	URL="$2"${client}
+	if [ -d "../../${client}" ]; then
+	echo "Já existe esse cliente"
+	fi
+	cp -rp  "../../${TARGET}" "../../${client}"
+	rm  "../../${client}/wp-config.php"
+	mv  "../../${client}/wp-config-model.php" "../../${client}/wp-config.php"
+	sed -i -e 's/usedbname/wp-${client}/g' "../../${client}/wp-config.php"
+	DBPASS = ` wp config get --constant=DB_PASSWORD`
+	DBUSER = ` wp config get --constant=DB_USER`
+	DBHOST = ` wp config get --constant=DB_HOST`
+	DBNAME = ` wp config get --constant=DB_NAME`
+
+	mysql -u "${DBUSER}" -h "${DBNAME}" -p${DBPASS} -e "create database ${DBNAME};"
+	mysqldump -u "${DBUSER}" -p${DBPASS} -h ${DBHOST} --compress --single-transaction wp-lpmodelo | mysql --host=${DBHOST} -f --user=${DBUSER} --password=${DBPASS} ${DBNAME}
+	 wp db query "update wp-config set option_value = '${URL}' where option_name = 'siteurl' or option_name = 'home'"
+done
