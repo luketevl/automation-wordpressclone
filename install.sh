850b352f90b6d848a806c92a9444c3f64c26aedf
luketevl <luketevl@gmail.com>
2017-11-09 13:00:37 -0200

    removed commas

--- install.sh
+++ install.sh
@@ -14,13 +14,13 @@ do
 	sed -i -e 's/usedbname/wp-${client}/g' "../../${client}/wp-config.php"
 	DBPASS = ` wp config get --allow-root --constant=DB_PASSWORD --path="../../${client}/"`
 	echo "${DBPASS}"
-	DBUSER = ` wp config get --allow-root --constant=DB_USER --path="../../${client}/"`
+	DBUSER = ` wp config get --allow-root --constant=DB_USER --path=../../${client}/`
 	echo "${DBUSER}"
-	DBHOST = ` wp config get --allow-root --constant=DB_HOST --path="../../${client}/"`
+	DBHOST = ` wp config get --allow-root --constant=DB_HOST --path=../../${client}/`
 	echo "${DBHOST}"
-	DBNAME = ` wp config get --allow-root --constant=DB_NAME --path="../../${client}/"`
+	DBNAME = ` wp config get --allow-root --constant=DB_NAME --path=../../${client}/`
 	echo "${DBNAME}"
-	
+
 	mysql -u "${DBUSER}" -h "${DBNAME}" -p${DBPASS} -e "create database ${DBNAME};"
 	mysqldump -u "${DBUSER}" -p${DBPASS} -h ${DBHOST} --compress --single-transaction wp-lpmodelo | mysql --host=${DBHOST} -f --user=${DBUSER} --password=${DBPASS} ${DBNAME}
 	 wp db query --allow-root "update wp-config set option_value = '${URL}' where option_name = 'siteurl' or option_name = 'home'"
